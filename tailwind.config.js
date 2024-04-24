2e2eeabafe40803c50e183ad9d0605957fe27983
Bekjan02 <75476548+Bekjan02@users.noreply.github.com>
2024-04-24 15:49:10 +0600

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'logo-color': ' #e4db00',
+        'yellow-second': ' #fffb98',
+        'yellow-light': ' #fffee9',
+        'bg-white': ' #fffffc',
+        'dark-logo': ' #2b292c',
+        'dark-text': ' #4d4d4d',
+        'grey-light': ' #a0a0a0',
+        dark: '#161616',
+      },
+      fontFamily: {
+        mulish: 'Mulish, sans-serif',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          'full-hd': '5rem',
+        },
+      },
+    },
+  },
+};
