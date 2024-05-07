429f0f0e799b487be9cb06d3dd05e091466a1076
Bekjan02 <bayganaliev@mail.ru>
2024-05-07 18:09:48 +0600

    update profile files and styles

--- pages/profile/myCompany/index.js
+++ pages/profile/myCompany/index.js
@@ -1,25 +1,3 @@
-// avatar upload
-const avatarInput = document.querySelector('#upload');
-const avatar = document.querySelector('#avatar');
-
-avatarInput.addEventListener('change', () => {
-    const reader = new FileReader();
-    reader.readAsDataURL(avatarInput.files[0]);
-    reader.addEventListener('load', () => {
-        avatar.src = reader.result;
-    });
-})
-
-// dropdown
-const dropdownBtn = document.querySelector('.dropdown__btn');
-const dropdownMenu = document.querySelector('.dropdown__menu');
-
-dropdownBtn.addEventListener('click', () => {
-    const isActive = dropdownMenu.classList.contains('active');
-    dropdownMenu.style.maxHeight = isActive ? "0" : `${dropdownMenu.scrollHeight}px`;
-    dropdownMenu.classList.toggle('active');
-})
-
 // photos upload
 const fileInputs = document.querySelectorAll('.photos__input');
 
