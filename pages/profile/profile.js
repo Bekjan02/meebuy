aca9ed9c2e70f8a3ae0a7bfc8c428ea9d9c4acfa
Bekjan02 <bayganaliev@mail.ru>
2024-05-08 16:50:36 +0600

    finished user's profile tenders page

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -19,3 +19,22 @@ dropdownBtn.addEventListener('click', () => {
     dropdownMenu.style.maxHeight = isActive ? "0" : `${dropdownMenu.scrollHeight}px`;
     dropdownMenu.classList.toggle('active');
 })
+
+// tabs
+function openTab(_, tabId) {
+    const contents = document.querySelectorAll('.tab-content');
+    contents.forEach(content => {
+        content.style.display = 'none';
+    });
+
+    const allTabs = document.querySelectorAll('.tabs__item');
+    allTabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    document.getElementById(tabId).style.display = 'block';
+    const activeTab = Array.from(allTabs).find(tab => tab.onclick.toString().includes(`'${tabId}'`));
+    if (activeTab) {
+        activeTab.classList.add('active');
+    }
+}
