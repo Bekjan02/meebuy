0628a412f40dbf56b7387031132c1d945f868503
Begimai7 <begikeldibekova@gmail.com>
2024-05-11 21:44:16 +0600

    processing

--- pages/suppliers/suppliers.js
+++ pages/suppliers/suppliers.js
@@ -3,6 +3,10 @@ const toggleLink = document.getElementById("toggleLink");
 const openIcon = document.querySelector(".openIcon");
 const closeIcon = document.querySelector(".closeIcon");
 const selectModal = document.getElementById("selectModal");
+const recommendedList = document.getElementById("recommended-list");
+const recommended = document.getElementById("recommended");
+const newSelectList = document.getElementById("new-list");
+const newSelect = document.getElementById("new");
 
 const toggleSelectModal = () => {
   if (
@@ -18,35 +22,45 @@ const toggleSelectModal = () => {
     closeIcon.style.display = "none";
   }
 };
-selectContainer.addEventListener("click", toggleSelectModal);
-
-const filterContainer = document.getElementById("filterContainer");
-const closeFilterContainer = document.getElementById("close-filter-container");
-const toggleFilter = document.getElementById("toggleFilter");
-const openFilter = document.getElementById("openFilter");
-const recommended = document.getElementById("recommended");
-const newSelect = document.getElementById("new");
-
-const toggleFilterModal = () => {
-  openFilter.style.display =
-    openFilter.style.display === "none" ? "block" : "none";
-};
-const toggleCloseFilterModal = () => {
-  openFilter.style.display = openFilter.style.display = "none";
-};
-const checkedFilter = (element) => {
+const checkedSelect = (element) => {
   const select = [recommended, newSelect];
-  for (let i = 0; i <= select.length; i++) {
+  for (let i = 0; i < select.length; i++) {
     if (select[i] === element) {
       select[i].style.display = "block";
+
+      setTimeout(() => {
+        selectModal.style.display = "none";
+        openIcon.style.display = "block";
+        closeIcon.style.display = "none";
+      }, 300);
     } else {
       select[i].style.display = "none";
     }
   }
 };
 
-recommended.addEventListener("click", () => checkedFilter(recommended));
-newSelect.addEventListener("click", () => checkedFilter(newSelect));
+recommendedList.addEventListener("click", () => {
+  checkedSelect(recommended);
+});
+newSelectList.addEventListener("click", () => {
+  checkedSelect(newSelect);
+});
+selectContainer.addEventListener("click", toggleSelectModal);
+
+// filter
+const filterContainer = document.getElementById("filterContainer");
+const closeFilterContainer = document.getElementById("close-filter-container");
+const toggleFilter = document.getElementById("toggleFilter");
+const openFilter = document.getElementById("openFilter");
+
+const toggleFilterModal = () => {
+  openFilter.style.display =
+    openFilter.style.display === "none" ? "block" : "none";
+};
+const toggleCloseFilterModal = () => {
+  openFilter.style.display = "none";
+};
+
 filterContainer.addEventListener("click", toggleFilterModal);
 closeFilterContainer.addEventListener("click", toggleCloseFilterModal);
 
@@ -59,3 +73,56 @@ document.addEventListener("click", (event) => {
     openFilter.style.display = "none";
   }
 });
+
+// dropdown mobile
+const dropdownBtn = document.querySelector(".dropdown__btn");
+const dropdownMenu = document.querySelector(".dropdown__menu");
+
+const openSort = () => {
+  const isActive = dropdownMenu.classList.contains("active");
+  dropdownMenu.style.maxHeight = isActive
+    ? "0"
+    : `${dropdownMenu.scrollHeight}px`;
+  dropdownMenu.classList.toggle("active");
+};
+
+dropdownBtn.addEventListener("click", openSort);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownItems = dropdownMenu.querySelectorAll(".dropdown__item");
+
+  dropdownItems.forEach(function (item) {
+    item.addEventListener("click", function (event) {
+      event.preventDefault();
+      const text = item.textContent;
+      changeButtonText(text);
+      openSort();
+    });
+  });
+});
+
+function changeButtonText(text) {
+  dropdownBtn.querySelector("p").textContent = text;
+}
+
+// sort dropdown desktop
+const all = document.getElementById("all");
+const suppliers = document.getElementById("suppliers");
+const manufactures = document.getElementById("manufactures");
+const logistics = document.getElementById("logistics");
+
+const toggleSort = (el) => {
+  const tabs = [all, suppliers, manufactures, logistics];
+
+  for (let i = 0; i <= tabs.length; i++) {
+    if (tabs[i] === el) {
+      tabs[i].style.backgroundColor = "#fffb98";
+    } else {
+      tabs[i].style.backgroundColor = "";
+    }
+  }
+};
+all.addEventListener("click", () => toggleSort(all));
+suppliers.addEventListener("click", () => toggleSort(suppliers));
+manufactures.addEventListener("click", () => toggleSort(manufactures));
+logistics.addEventListener("click", () => toggleSort(logistics));
